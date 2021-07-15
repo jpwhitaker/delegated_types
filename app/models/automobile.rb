466a75391a1510e7ecc6688247ce6948b67475f1
JPW <jpw@hawaii.edu>
2021-07-15 13:41:42 -1000

    added instanxce methods that call automobile for missing fields

--- app/models/automobile.rb
+++ app/models/automobile.rb
@@ -2,7 +2,7 @@ class Automobile < ApplicationRecord
   belongs_to :user
   delegated_type :autoable, types: %w[ Automobile LuxuryAutomobile ]
   include Autoable
-  delegate :custom_monogram, to: :autoable
+  # delegate :custom_monogram, to: :autoable
 
 
 end
--- app/models/luxury_automobile.rb
+++ app/models/luxury_automobile.rb
@@ -1,5 +1,10 @@
 class LuxuryAutomobile < ApplicationRecord
   include Autoable
+  delegate :num_wheels, to: :automobile
 
+  def num_wheels= number
+    self.automobile.num_wheels = number
+    self.save!
+  end
 
 end
